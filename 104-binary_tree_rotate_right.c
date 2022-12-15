f310be7424940aab2908e541c3cb0a4375f40779
Mohamed Ayman <mohayman2007@gmail.com>
2022-12-15 22:30:10 +0000

    Project 0x1D - Advanced Tasks 22 & 23

---
+++ 104-binary_tree_rotate_right.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_rotate_right - Right-rotate a binary tree
+ * @tree: Pointer to the root node
+ *
+ * Return: Pointer to the new root or NULL if tree is NULL
+ */
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
+{
+	binary_tree_t *new_root;
+
+	if (!tree)
+		return (NULL);
+
+	if (!tree->left)
+		return (tree);
+
+	new_root = tree->left;
+	new_root->parent = NULL;
+	tree->left = new_root->right;
+	if (tree->left)
+		tree->left->parent = tree;
+	new_root->right = tree;
+	tree->parent = new_root;
+
+	return (new_root);
+}
