26842f8f20c2164ba31d4d9be9f50bf99857822e
Mohamed Ayman <mohayman2007@gmail.com>
2022-12-15 17:29:00 +0000

    Project 0x1D - Mandatory Tasks 14 --> 18

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -33,6 +33,9 @@ size_t binary_tree_height_extended(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	if (!tree)
+		return (0);
+
 	return (binary_tree_height_extended(tree->left) -
 		binary_tree_height_extended(tree->right));
 }
