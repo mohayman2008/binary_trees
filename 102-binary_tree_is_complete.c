3f018146755130a6dcaffb79993ff0157a05323d
Mohamed Ayman <mohayman2007@gmail.com>
2022-12-19 01:36:41 +0000

    Project 0x1D - Advanced - Task 21 (different approach)

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,54 +1,125 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_size - measures the size of a binary tree
- *
- * @tree: tree root
- * Return: size of the tree or 0 if tree is NULL;
+ * print_tree_array - Prints tree nodes from array of tree nodes pointers
+ * @array: The array of tree nodes pointers
+ * @size: The size of the array
  */
-size_t binary_tree_size(const binary_tree_t *tree)
+void print_tree_array(binary_tree_t **array, size_t size)
 {
-	if (tree == NULL)
-		return (0);
+	size_t i = 0;
 
-	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+	if (!array)
+	{
+		printf("Empty array!\n");
+		return;
+	}
+	for (; i < size ; i++)
+	{
+		printf("\t%lu: ", i);
+		if (!(array[i]))
+			printf("NULL\n");
+		else
+		{
+			printf("value = %d | ", array[i]->n);
+			if (array[i]->parent)
+				printf("parent = %d at %p | ", array[i]->parent->n,
+					(void *) array[i]->parent);
+			else
+				printf("parent is NULL | ");
+			if (array[i]->left)
+				printf("left = %d at %p | ", array[i]->left->n,
+					(void *) array[i]->left);
+			else
+				printf("left is NULL | ");
+			if (array[i]->right)
+				printf("right = %d at %p\n", array[i]->right->n,
+					(void *) array[i]->right);
+			else
+				printf("right is NULL\n");
+		}
+	}
+}
+
+
+/**
+ * check_level_complete - Traverse a binary tree level and check if it
+ *				causes the tree not to be complete
+ * @level_nodes: Array of nodes in the level
+ *
+ * Return: Dynamically allocated array of the nodes in the next level
+ */
+binary_tree_t **check_level_complete(binary_tree_t **level_nodes)
+{
+	binary_tree_t **nodes_list, **itr;
+	size_t len = 0, i = 0;
+	int no_more = 0;
+
+	if (!level_nodes)
+		return (NULL);
+	while (level_nodes[len])
+		len++;
+	nodes_list = malloc(sizeof(*nodes_list) * (len * 2 + 1));
+	if (!nodes_list)
+		return (NULL);
+
+	for (itr = level_nodes ; *itr ; itr++)
+	{
+		if ((no_more && ((*itr)->left || (*itr)->right)) ||
+			((*itr)->right && !(*itr)->left))
+		{
+			nodes_list[0] = nodes_list[i++] = NULL;
+			break;
+		}
+		else if (!(*itr)->left)
+			no_more = 1;
+		else if (!(*itr)->right)
+			nodes_list[i++] = (*itr)->left,
+			no_more = 1;
+		else
+			nodes_list[i++] = (*itr)->left,
+			nodes_list[i++] = (*itr)->right;
+	}
+	nodes_list[i] = NULL, free(level_nodes);
+
+	for (itr = nodes_list ; *itr && no_more && i ; itr++)
+		if ((*itr)->left || (*itr)->right)
+			nodes_list[0] = NULL;
+
+	if (!*nodes_list && i)
+		free(nodes_list), nodes_list = NULL;
+	else if (no_more)
+		nodes_list[0] = NULL;
+	return (nodes_list);
 }
 
 /**
- * tree_is_complete - checks if tree is complete
+ * binary_tree_is_complete - Checks if a binary tree is complete
+ * @tree: Pointer to the root of the tree
  *
- * @tree: pointer to the tree root
- * @i: node index
- * @cnodes: number of nodes
- * Return: 1 if tree is complete, 0 otherwise
- */
-int tree_is_complete(const binary_tree_t *tree, int i, int cnodes)
-{
-	if (tree == NULL)
-		return (1);
-
-	if (i >= cnodes)
-		return (0);
-
-	return (tree_is_complete(tree->left, (2 * i) + 1, cnodes) &&
-		tree_is_complete(tree->right, (2 * i) + 2, cnodes));
-}
-
-
-/**
- * binary_tree_is_complete - calls to tree_is_complete function
- *
- * @tree: tree root
- * Return: 1 if tree is complete, 0 otherwise
+ * Return: (1) if the tree is complete or (0) otherwise
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-	size_t cnodes;
+	binary_tree_t **nodes_list;
 
-	if (tree == NULL)
+	if (!tree)
 		return (0);
 
-	cnodes = binary_tree_size(tree);
+	nodes_list = malloc(sizeof(*nodes_list) * 2);
+	if (!nodes_list)
+		return (0);
 
-	return (tree_is_complete(tree, 0, cnodes));
+	nodes_list[0] = (binary_tree_t *) (long int) tree;
+	nodes_list[1] = NULL;
+
+	while (*nodes_list)
+	{
+		nodes_list = check_level_complete(nodes_list);
+		if (!nodes_list)
+			return (0);
+	}
+
+	free(nodes_list);
+	return (1);
 }
